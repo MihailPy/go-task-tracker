c9513d5192e9a452e1b6a69a90bf163d95d2c6ef
MihailPy <mihailpy02@gmail.com>
2026-02-12 18:01:02 +0300

    Added enums for Status in Task.

--- main.go
+++ main.go
@@ -7,12 +7,20 @@ import (
 	"time"
 )
 
+type TaskStatus string
+
+const (
+	StatusTodo       TaskStatus = "todo"
+	StatusInProgress TaskStatus = "in-progress"
+	StatusDone       TaskStatus = "done"
+)
+
 type Task struct {
-	Id          int       `json:"id"`
-	Description string    `json:"description"`
-	Status      string    `json:"status"`
-	CreatedAt   time.Time `json:"createdAt"`
-	UpdatedAt   time.Time `json:"updatedAt"`
+	Id          int        `json:"id"`
+	Description string     `json:"description"`
+	Status      TaskStatus `json:"status"`
+	CreatedAt   time.Time  `json:"createdAt"`
+	UpdatedAt   time.Time  `json:"updatedAt"`
 }
 
 func SaveTasks(fileName string, tasks []Task) error {
